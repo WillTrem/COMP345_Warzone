7d7a753c5cc12e616d73500dfdaa65831787d581
DouceNouni <roxy.perruche@hotmail.com>
2023-09-15 13:43:03 -0400

    Basic project framework.

---
+++ Assignment_1/Assignment_1.cpp
@@ -0,0 +1,12 @@
+﻿// Assignment_1.cpp : Defines the entry point for the application.
+//
+
+#include "Assignment_1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
