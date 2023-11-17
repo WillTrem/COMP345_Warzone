5d8cb991666bb3aa9186e563b40f5fbda7ce523e
DouceNouni <roxy.perruche@hotmail.com>
2023-11-17 16:32:48 -0500

    First pass at most of the strategies' toAttack() and toDefend() functions.

--- Assignment_1/PlayerStrategy.h
+++ Assignment_1/PlayerStrategy.h
@@ -7,6 +7,8 @@
 
 #include <string>
 #include <algorithm>
+#include <vector>
+#include <set>
 
 #include "Cards.h"
 #include "Map.h"
@@ -23,7 +25,7 @@ int NUM_TO_DEFEND = 5; // The number of territories AI players will targer/prior
 // Abstract strategy class
 class PlayerStrategy
 {
-private:
+protected: // So that it remain accessible by child classes.
     Player *p;
 
 public:
@@ -39,6 +41,10 @@ public:
     virtual vector<Territory *> toDefend();
 
     virtual void issueOrder(Order *order);
+
+
+    // Utility methods.
+    vector<Territory*> getAdjacentTerritories(); // Returns a vector of all enemy territories adjacent to the player's.
 };
 
 
@@ -56,12 +62,69 @@ public:
     void issueOrder(Order* order);
 };
 
+// Aggressive Player Strategy.
+// "Computer player that focuses on attack (deploys or advances armies on its strongest country, then always advances to enemy territories until it cannot do so anymore; will use any card with an aggressive purpose.)"
+class AggressivePlayerStrategy : public PlayerStrategy
+{
+public:
+    // Constructor(s)
+
+    vector<Territory*> toAttack();
+
+    vector<Territory*> toDefend();
+
+    void issueOrder(Order* order);
+};
+
+// Benevolent Player Strategy.
+// "Computer player that focuses on protecting its weak countries (deploys or advances armies on its weakest countries, never advances to enemy territories; may use cards but will never use a card in a way that purposefully will harm anyone)."
+class BenevolentPlayerStrategy : public PlayerStrategy
+{
+public:
+    // Constructor(s)
+
+    vector<Territory*> toAttack();
+
+    vector<Territory*> toDefend();
+
+    void issueOrder(Order* order);
+};
+
+// Neutral Player Strategy.
+// "Computer player that never issues any order, nor uses any cards, though it may have or receive cards.If a Neutral player is attacked, it becomes an Aggressive player."
+class NeutralPlayerStrategy : public PlayerStrategy
+{
+public:
+    // Constructor(s)
+
+    vector<Territory*> toAttack();
+
+    vector<Territory*> toDefend();
+
+    void issueOrder(Order* order);
+};
+
+// Cheater Player Strategy.
+// "Computer player that automatically conquers all territories that are adjacent to its own territories(only once per turn). Does not use cards, though it may have or receive cards."
+class CheaterPlayerStrategy : public PlayerStrategy
+{
+public:
+    // Constructor(s)
+
+    vector<Territory*> toAttack();
+
+    vector<Territory*> toDefend();
+
+    void issueOrder(Order* order);
+};
 
 
-// Utility methods and structs.
+// Utility structs.
 
 // TO DO: DELETE UNUSED.
+
 // Used to sort a vector of territories by the number of troops present on them.
+// Used by the Aggressive player's toAttack() method.
 struct LessThan_TroopsPresent 
 {
     inline bool operator() (const Territory& A, const Territory& B);
@@ -89,6 +152,7 @@ struct LessThan_AdjacentEnemyTroops
 
 // Used to sort a vector of territories by the number of adjacent enemy troops, then the number of one's own units present on them.
 // More adjacent enemy troops, fewer owned troops = weaker territory.
+// Used by the Benevolent Player's toDefend() method.
 struct LessThan_AdjacentEnemyTroops_TroopsPresent
 {
     inline bool operator() (const Territory& A, const Territory& B);
