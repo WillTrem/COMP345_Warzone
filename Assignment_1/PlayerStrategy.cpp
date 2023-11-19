f94c62faf7f7d68ec4c3e72486ee2ed0116e1fe4
Shawn Cui <85511543+S-Netizen49@users.noreply.github.com>
2023-11-19 14:21:51 -0500

    player strat cpp init

---
+++ Assignment_1/PlayerStrategy.cpp
@@ -0,0 +1,12 @@
+#include "PlayerStrategy.h"
+#include <string>
+
+using namespace std;
+
+class concreteAggressivePlayer : public PlayerStrategy
+{
+public:
+    void vector<Territory *> toAttack(){
+
+    };
+};
