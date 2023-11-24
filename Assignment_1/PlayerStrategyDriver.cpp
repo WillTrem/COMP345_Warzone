815091584dcc4b7dd3a8fcfe69f05fcee7976c27
Shawn Cui <85511543+S-Netizen49@users.noreply.github.com>
2023-11-23 19:54:26 -0500

    minor playerstrategydriver update

--- Assignment_1/PlayerStrategyDriver.cpp
+++ Assignment_1/PlayerStrategyDriver.cpp
@@ -2,7 +2,7 @@
 
 void testPlayerStrategies()
 {
-    
+
     // Player Creation
     Player HumanPlayer("Timmy");
     Player AngryBot("");
@@ -10,18 +10,32 @@ void testPlayerStrategies()
     Player NeutralBot("");
     Player CheatBot("");
 
+    // Order Creation
+
     // Setting the strategy for each player
     AngryBot.setStrategy(new AggressivePlayerStrategy());
     NiceBot.setStrategy(new BenevolentPlayerStrategy());
     NeutralBot.setStrategy(new NeutralPlayerStrategy());
     CheatBot.setStrategy(new CheaterPlayerStrategy());
 
-    std::cout << "reached 1" << endl;
+    std::cout << "reached toAttack()" << endl;
     // Testing toAttack() method with all computer players
     AngryBot.toAttack();
     NiceBot.toAttack();
     NeutralBot.toAttack();
     CheatBot.toAttack();
 
-    std::cout << "reached 2" << endl;
+    // std::cout << "reached toDefend()" << endl;
+    // // Testing toDefend() method will all computer players
+    // AngryBot.toDefend();
+    // NiceBot.toDefend();
+    // NeutralBot.toDefend();
+    // CheatBot.toDefend();
+
+    // std::cout << "reached issueOrder(order)" << endl;
+    // // Testing toDefend() method will all computer players
+    // AngryBot.issueOrder();
+    // NiceBot.issueOrder();
+    // NeutralBot.issueOrder();
+    // CheatBot.issueOrder();
 }
